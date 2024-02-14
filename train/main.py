37fbeab745c4c1cc3dae3d63a3adf013ff6d2acb
s268588@studenti.polito.it <s268588@studenti.polito.it>
2024-02-14 22:47:12 +0100

    train again

--- train/main.py
+++ train/main.py
@@ -459,7 +459,7 @@ def main(args):
             return model
 
         #print(torch.load(args.state))
-        if args.pretrained_net:
+        if args.pretrained_net_flag:
              model.load_state_dict(torch.load(args.pretrained_net))
         else:
              model = load_my_state_dict(model, torch.load(args.state))
@@ -539,6 +539,7 @@ if __name__ == '__main__':
     parser.add_argument('--iouVal', action='store_true', default=True)  
     parser.add_argument('--resume', action='store_true')    #Use this flag to load last checkpoint for training  
 
-    parser.add_argument('--pretrained_net', default='../trained_models/erfnet_pretrained.pth')  
+    parser.add_argument('--pretrained_net', default='../trained_models/erfnet_pretrained.pth') 
+    parser.add_argument('--pretrained_net_flag', action='store_true', default=False)   
     
     main(parser.parse_args())
